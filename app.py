b2cb26e4509bfbbc023818352cb8585a49e74286
ovavellan <oscaravellan2000@gmail.com>
2022-05-26 22:00:20 -0500

    commit 1

---
+++ app.py
@@ -0,0 +1,19 @@
+
+#importar libreria de Flask
+from tkinter import SW
+from flask import Flask, render_template
+
+#Iniciar variable de aplicacion 
+app= Flask(__name__, template_folder='templates')
+
+#decorador para definir la ruta raíz e index 
+@app.route('/' )
+def principal():
+    return "Hola Mundo"
+    return render_template('index.html')
+
+
+# main del programa 
+if __name__ == '__main__':
+    #debug = true, para reiniciar automaticamente el servidor , tiemo de desarrollo 
+    app.run(debug=True)
